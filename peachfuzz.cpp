46d7ecdf04d4bff40ced7d60a316df61883beb4e
oxagast <marshallwhittaker@gmail.com>
2015-08-19 23:44:37 -0400

    deleted:    Makefile 	new file:   configure 	renamed:    gzstream/gzstream.o -> gzstream/libgzstream.a 	modified:   peachfuzz.cpp

--- peachfuzz.cpp
+++ peachfuzz.cpp
@@ -93,14 +93,11 @@ bool file_exists(const std::string& filen) {
 
 
 int rand_me_plz (int rand_from, int rand_to) {
-  
   std::random_device rd;
   std::default_random_engine generator(rd());   // seed random
   std::uniform_int_distribution<int> distribution(rand_from,rand_to);  // get a random
   auto roll = std::bind ( distribution, generator );  // bind it so we can do it multiple times
-  return(roll());  
-  
-  
+  return(roll());
 }
 
 
