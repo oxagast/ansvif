fc6aae1ebed95b57a1e93c6212ba754c1dd50cdd
Marshall Lee Whittaker <marshallwhittaker@gmail.com>
2016-10-09 09:00:18 -0400

    Fixed the randomization code under linux to use random_device.

--- src/randomizer.cpp
+++ src/randomizer.cpp
@@ -1,7 +1,18 @@
 #include <random>
-
+#include <functional>
 int rand_me_plz(int rand_from, int rand_to) {
+#ifdef _WIN32
   int first = rand_to-(rand_from-1);
-  int roll = rand()% first - rand_from;
-  return (roll);
+  int rolled = rand()% first - rand_from;
+#endif
+#ifdef __linux
+    std::random_device rd;
+  std::default_random_engine generator(rd()); // seed random
+  std::uniform_int_distribution<int> distribution(rand_from,
+                                                  rand_to); // get a random
+  auto roll = std::bind(distribution,
+                        generator); // bind it so we can do it multiple times
+int rolled = roll();
+#endif
+  return (rolled);
 }
