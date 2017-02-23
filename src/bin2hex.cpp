6fe32578c918b8516c9603552fbc49ba310fe0c6
Marshall Lee Whittaker <marshallwhittaker@gmail.com>
2017-02-23 06:51:42 -0500

    More code cleanup.

---
+++ src/bin2hex.cpp
@@ -0,0 +1,117 @@
+/*
+ * ansvif
+ * A Not So Very Intelligent Fuzzer
+ * Marshall Whittaker / oxagast
+ */
+
+//    __ _  _  __   ___  __  ____ ____ 
+//   /  ( \/ )/ _\ / __)/ _\/ ___(_  _)
+//  (  O )  (/    ( (_ /    \___ \ )(  
+//   \__(_/\_\_/\_/\___\_/\_(____/(__)
+
+#include <iomanip>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+
+std::string binstr_to_hex(std::string bin_str) {
+  std::stringstream hex_out;
+  /* initialize our string to put it in with a filler of
+   * zero and upercase hexadecimal with a width of 2
+   */
+  std::string hexxy;
+  hex_out << std::setw(2) << std::setfill('0') << std::hex
+          << std::uppercase;
+#ifdef _WIN32
+          std::copy(bin_str.begin(), bin_str.end(),
+            std::ostream_iterator<unsigned int>(
+                hex_out, "\\x"));
+  if (hex_out.str() != "") {
+    /* if we don't have anything in the buffer throw
+     * a space in
+     */
+    hexxy = hex_out.str() + "20";
+  }
+  hexxy = "\\x" + hexxy;
+  if (hexxy == "\\x20\\x20") {
+    return "";
+  } else {
+    /* return to be fed into printf */
+    return (hexxy);
+  }
+#endif
+#ifdef __linux
+  std::copy(bin_str.begin(), bin_str.end(),
+            std::ostream_iterator<unsigned int>(
+                hex_out, "\\\\x"));
+  if (hex_out.str() != "") {
+    hexxy = hex_out.str() + "20";
+  }
+  /* make sure that it begins in \\x for output */
+  hexxy = "\\\\x" + hexxy;
+  /* if the output is only spaces then return nothing */
+  if (hexxy == "\\\\x20\\\\x20") {
+    return "";
+  } 
+  /* return to sys_string in hex */
+  else {
+    return (hexxy);
+  }
+#endif
+}
+
+std::string binstr_to_hex_pc(std::string bin_str) {
+  /* initialize the hex to go out */
+  std::stringstream hex_out;
+  /* initialize our string to put it in with a filler of
+   * zero and upercase hexadecimal with a width of 2
+   */
+  std::string hexxy;
+  hex_out << std::setw(2) << std::setfill('0') << std::hex
+          << std::uppercase;
+#ifdef _WIN32
+  /* for each of them put a % in front for the output to printf
+   * if we don't have anything in the buffer then we'll just put
+   * a space
+   */
+  std::copy(bin_str.begin(), bin_str.end(),
+            std::ostream_iterator<unsigned int>(
+                hex_out, "%"));
+  if (hex_out.str() != "") {
+    hexxy = hex_out.str() + "20";
+  }
+  hexxy = "%" + hexxy;
+  if (hexxy == "%20%20") {
+    return "";
+  } else {
+    /* return the hex */
+    return (hexxy);
+  }
+#endif
+#ifdef __linux
+  /* for each character put the % infront for the output of
+   * printf if we don't have anything in the buffer we'll
+   * put a space
+   */        
+  std::copy(bin_str.begin(), bin_str.end(),
+    std::ostream_iterator<unsigned int>(hex_out, "%"));
+  if (hex_out.str() != "") {
+    hexxy = hex_out.str() + "20";
+  }
+  /* make sure it starts with a % in this case */
+  hexxy = "%" + hexxy;
+  /* if nothing is in it but hex spaces then we will
+   * return nothing
+   */
+  if (hexxy.substr(0,3) == "%20") {
+    return "";
+  } 
+  /* if there is stuff in the variable the return sys_string
+   * in hex
+   */
+  else {
+    return (hexxy);
+  }
+#endif
+}
--- src/sys_string.cpp
+++ src/sys_string.cpp
@@ -15,6 +15,7 @@
 #include <vector>
 
 std::string binstr_to_hex(std::string bin_str);
+std::string binstr_to_hex_pc(std::string bin_str_pc);
 std::vector<std::string>
 get_out_str(std::string env_str, std::string valgrind_str, std::string sys_str,
             std::string path_str, std::string always_arg_b, std::string always_arg,
@@ -82,6 +83,78 @@ get_out_str(std::string env_str, std::string valgrind_str, std::string sys_str,
    * printf compatible stuff into the vector to be fed back into
    * the calling routine
    */
+  std::vector<std::string> out_all;
+  out_all.push_back(out_str);
+  out_all.push_back(out_str_p);
+  return (out_all);
+}
+
+std::vector<std::string>
+get_out_str_pc(std::string env_str, std::string valgrind_str,
+               std::string sys_str, std::string path_str,
+               std::string always_arg_b, std::string always_arg, 
+               std::string fuzz_after, std::string log_prefix) {
+  /* these are the strings that will go to be run in popen
+   * out_str is normal, and out_str_p is pritnf compatible
+   * for easy crash replay, except this is for % for
+   * web browsers and such
+   */
+  std::string out_str, out_str_p;
+  /* make sure we're not shooting blanks */
+#ifdef __linux
+  if (sys_str != "") {
+    if (env_str != "") {
+      /* this is if we have an environment string */
+      out_str_p =
+          "$(printf \"" + binstr_to_hex(env_str) + "\") " + valgrind_str + " " +
+          path_str + " $(printf \"" + binstr_to_hex(binstr_to_hex_pc(sys_str)) +
+          "\") " + always_arg + binstr_to_hex(binstr_to_hex_pc(fuzz_after));
+    }
+    if (env_str == "") {
+      /* and if we don't have environment variables to fuzz */
+      out_str_p = valgrind_str + " " + path_str + " " + always_arg_b + 
+                  " $(printf \"" + binstr_to_hex(binstr_to_hex_pc(sys_str)) + 
+                  "\") " + always_arg + " $(printf \"" +
+                  binstr_to_hex(binstr_to_hex_pc(fuzz_after)) +
+                  "\") ";
+    }
+    out_str = env_str + " " + valgrind_str + " " + path_str + " " +
+              always_arg_b + binstr_to_hex_pc(sys_str) + " " + always_arg +
+              binstr_to_hex_pc(fuzz_after);
+  }
+  if (log_prefix == "") {
+    /* incase we are logging don't leave a blank file */
+    out_str = out_str + " >/dev/null 2>&1; echo $?";
+  }
+  else {
+    /* get the signal here and log */
+    out_str = out_str + " >" + log_prefix + ".output.ansvif.log 2>&1; echo $?";
+  }
+  /* we're putting the normal version and the printf
+   * version in this vector, normal first, printf second,
+   * then feeding it back to the calling routing
+   */
+#endif
+#ifdef _WIN32
+  if (sys_str != "") {
+    if (env_str != "") {
+      out_str_p = " (.\\printf.exe \\x" + binstr_to_hex(env_str) + "\") " +
+                  "'" + path_str + "' (.\\printf.exe \\x" +
+                  binstr_to_hex(sys_str) + ")" + always_arg + " " + fuzz_after +
+                  "; echo $LastExitCode";
+    }
+    if (env_str == "") {
+      out_str_p = "'" + path_str + "' (.\\printf.exe \\x" +
+                  binstr_to_hex(sys_str) + ") " + always_arg + " " +
+                  fuzz_after +
+                  "; echo $LastExitCode";
+    }
+   out_str = "powershell -c " + env_str + "(Start-Job {& '" + path_str + "' " + always_arg_b + " " + sys_str + " " +
+              always_arg + " " + fuzz_after;
+  }
+  out_str = out_str + " > " + log_prefix +
+            ".output.ansvif.log; echo $LastExitCode}";
+#endif
   std::vector<std::string> out_all;
   out_all.push_back(out_str);
   out_all.push_back(out_str_p);
