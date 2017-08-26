ed3a766b9fbb0ff0b93d77506a2f1985f5d40c89
oxagast <marshallwhittaker@gmail.com>
2017-08-26 17:59:36 -0400

    Some commenting in ansvif_gtk.cpp

--- src/ansvif_gtk.cpp
+++ src/ansvif_gtk.cpp
@@ -81,6 +81,7 @@ int set_buffer_size(GtkWidget *buf_size_zero, gpointer data) {
 void destroy(GtkWidget *widget, gpointer data) { gtk_main_quit(); }
 
 int main(int argc, char *argv[]) {
+  /* Declare some vars */
   std::string command;
   GtkWidget *window;
   GtkWidget *opters;
@@ -88,36 +89,45 @@ int main(int argc, char *argv[]) {
   GtkWidget *command_sel;
   GtkWidget *template_sel;
   GtkWidget *buf_size_zero;
+  /* Pull in the args for gtk */
   gtk_init(&argc, &argv);
+  /* Create gtk window */
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  /* Set the title to ansvif */
   gtk_window_set_title(GTK_WINDOW(window), "ansvif");
   g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
   gtk_container_set_border_width(GTK_CONTAINER(window), 15);
+  /* Make a container for our buttons */
   opters = gtk_fixed_new();
   gtk_container_add(GTK_CONTAINER(window), opters);
+  /* Make the allmighty fuzz button */
   fuzz_it = gtk_button_new_with_label("Fuzz!");
   g_signal_connect(GTK_OBJECT(fuzz_it), "clicked", G_CALLBACK(fuzz_call),
                    "fuzz_it");
   gtk_fixed_put(GTK_FIXED(opters), fuzz_it, 400, 10);
   gtk_widget_show(fuzz_it);
-//  gtk_widget_show(opters);
+  /*  Make our template file selection */
   template_sel = gtk_button_new_with_label("Select Template");
   g_signal_connect(GTK_OBJECT(template_sel), "clicked",
                    G_CALLBACK(select_template), "template_sel");
   gtk_fixed_put(GTK_FIXED(opters), template_sel, 50, 80);
   gtk_widget_show(template_sel);
+  /* Make our binary to be fuzzed file selection */
   command_sel = gtk_button_new_with_label("Select binary to fuzz");
   g_signal_connect(GTK_OBJECT(command_sel), "clicked",
                    G_CALLBACK(select_binary), "command");
   gtk_fixed_put(GTK_FIXED(opters), command_sel, 50, 50);
   gtk_widget_show(command_sel);
+  /* A toggle for turning buffer size 0 on and off */
   buf_size_zero = gtk_toggle_button_new_with_label("Buffer Size 0");
   g_signal_connect(GTK_OBJECT(buf_size_zero), "clicked",
                    G_CALLBACK(set_buffer_size), "buf_size_zero");
   gtk_fixed_put(GTK_FIXED(opters), buf_size_zero, 50, 200);
   gtk_widget_show(buf_size_zero);
+  /* Show that part of the screen */
   gtk_widget_show(opters);
+  /* Show the whole window at once */
   gtk_widget_show_all(window);
   gtk_main();
-  return 0;
+  return(0);
 }
