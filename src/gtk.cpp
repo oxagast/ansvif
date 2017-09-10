f24ffcf3940fb35a661780e81bde0791813582c3
oxagast <marshallwhittaker@gmail.com>
2017-09-10 11:38:18 -0400

    Added a way to specify files for ansvif_gtk. -p [path] (for the path and -l (local)

--- src/gtk.cpp
+++ src/gtk.cpp
@@ -57,11 +57,12 @@ GtkWidget *template_sel_t;
 GtkWidget *log_sel_t;
 GtkWidget *oo_sel_t;
 std::string ver = " -i ";
+std::string ansvif_loc = "ansvif ";
 
 static void destroy(GtkWidget *widget, gpointer *data) { gtk_main_quit(); }
 
 std::string ansvif_str() {
-  ansvif_call = "./ansvif " + ver + random_data + random_buffer_s + buffer_size +
+  ansvif_call = ansvif_loc + ver + random_data + random_buffer_s + buffer_size +
                 log_file + maximum_args + other_seperator + binary_file +
                 environment_file + template_file + crash_code + other_options_file;
   /* damn son */
@@ -317,7 +318,23 @@ int main(int argc, char *argv[]) {
   GtkWidget *oo_sel;
   GtkWidget *log_sel;
   gint tmp_pos;
+  int c;
 
+  while ((c = getopt (argc, argv, "lp:")) != -1) {
+    switch (c)
+      {
+      case 'l':
+        ansvif_loc = "./ansvif";
+        break;
+      case 'p':
+        ansvif_loc = optarg;
+        break;
+          default:
+        abort ();
+      }
+  }
+  
+  
   random_buffer_size = gtk_check_button_new_with_label("Random Buffer Size");
   buf_size_zero = gtk_check_button_new_with_label("Buffer Size 0");
   /* Pull in the args for gtk */
