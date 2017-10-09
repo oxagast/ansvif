2ae8b1fafebb00c6aba2f9784ee0eee241524a1e
Marshall Lee Whittaker <marshall@localhost.localdomain>
2017-10-09 16:45:11 -0400

    More reliable buffer overflow in make check.

---
+++ src/generic_buffer_overflow.c
@@ -0,0 +1,26 @@
+/*
+ * ansvif
+ * A Not So Very Intelligent Fuzzer
+ * Marshall Whittaker / oxagast
+ *
+ *
+ *    __ _  _  __   ___  __  ____ ____ 
+ *  /  ( \/ )/ _\ / __)/ _\/ ___(_  _)
+ * (  O )  (/    ( (_ /    \___ \ )(  
+ *  \__(_/\_\_/\_/\___\_/\_(____/(__)
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+void func(char **argv) {
+  printf("running strcpy...\n");
+  char arr[16];
+  strcpy(arr, argv[2]);
+  return;
+}
+int main(int argc, char *argv[]) {
+  func(argv);
+  return(0);
+}
