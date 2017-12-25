505c42660aa207f9198a7d88f68dc3b0524beab1
Marshall Whittaker <marshallwhittaker@gmail.com>
2017-12-24 21:03:50 -0500

    Added a .bat script to compile ansvif under Windows 10.

---
+++ make_win.bat
@@ -0,0 +1,8 @@
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir "C:\Program Files\mingw-w64\*posix-seh*" /b /S`) DO (
+SET mingw=%%F
+)
+set PATH=%mingw%\mingw64\bin;%PATH%
+echo %PATH%
+"windres.exe" bin\win\metadata.rc -O coff -o metadata.res
+"g++.exe" src/common.cpp src/bin2hex.cpp src/popen2.cpp src/main.cpp src/help.cpp src/match_fault.cpp src/sys_string.cpp src/man_read.cpp src/randomizer.cpp src/trash.cpp src/log.cpp metadata.res src/version.h -I./ -I./include -std=c++11 -lstdc++ -lpthread -O2 -o ansvif.exe -static -static-libgcc -static-libstdc++
+"gcc.exe" src/win/printf.c -o printf.exe
