66be2d530a57a4e8329611dcd1e25cbb4a189c97
Marshall Lee Whittaker <marshall@likon.maskster.site>
2019-10-08 19:07:09 -0400

    Fixed header.

--- src/null_pointer.c
+++ src/null_pointer.c
@@ -1,3 +1,15 @@
+/*
+ * ansvif
+ * A Not So Very Intelligent Fuzzer
+ * Marshall Whittaker / oxagast
+ *
+ *
+ *   __ _  _  __   ___  __  ____ ____
+ *  /  ( \/ )/ _\ / __)/ _\/ ___(_  _)
+ * (  O )  (/    ( (_ /    \___ \ )(
+ *  \__(_/\_\_/\_/\___\_/\_(____/(__)
+ */
+
 int main() {
   /* this is bad, i'm calling on a null pointer */
   char *assigned_null = 0;
